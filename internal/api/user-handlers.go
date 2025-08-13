6617e32b5871935b1f360f1d84806ee8f122fc91
Grégory Alvim <gregory.pereira@pacificsec.com>
2025-08-13 17:42:15 -0300

    feat: initial setup

---
+++ internal/api/user-handlers.go
@@ -0,0 +1,9 @@
+package api
+
+import "net/http"
+
+func (api *Api) handleSignUpUser(w http.ResponseWriter, r *http.Request) {}
+
+func (api *Api) handleLoginUser(w http.ResponseWriter, r *http.Request) {}
+
+func (api *Api) handleLogoutUser(w http.ResponseWriter, r *http.Request) {}
---
+++ readme
@@ -0,0 +1,1 @@
+air --build.cmd "go build -o ./bin/api ./cmd/api" --build.bin "./bin/api"
