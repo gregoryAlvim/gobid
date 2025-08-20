55bf98d18e824e7ba7a37a8b0ef30722aedbefb5
Grégory Alvim <gregori.jagua@gmail.com>
2025-08-20 18:51:19 -0300

    feat: init create user use case

--- internal/api/user-handlers.go
+++ internal/api/user-handlers.go
@@ -1,8 +1,18 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
 
-func (api *Api) handleSignUpUser(w http.ResponseWriter, r *http.Request) {}
+	"github.com/gregoryAlvim/gobid/internal/jsonutils"
+	"github.com/gregoryAlvim/gobid/internal/usecase/user"
+)
+
+func (api *Api) handleSignUpUser(w http.ResponseWriter, r *http.Request) {
+	data, problems, err := jsonutils.DecodeValidJson[user.CreateUserReq](r)
+	if err != nil {
+		_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+	}
+}
 
 func (api *Api) handleLoginUser(w http.ResponseWriter, r *http.Request) {}
 
